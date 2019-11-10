0feff89f8a0932678672c14247cc0c8a4eb363f3
Guilherme Rosa <fc47902@alunos.fc.ul.pt>
2019-11-10 18:41:55 +0000

    add client

---
+++ client/src/main/java/ulisboa/tecnico/sirs/client/App.java
@@ -0,0 +1,13 @@
+package ulisboa.tecnico.sirs.client;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
