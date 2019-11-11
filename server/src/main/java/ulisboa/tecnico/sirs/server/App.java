57dba621a7c42e31ef97e1c06c58795ddcf15302
Guilherme Rosa <fc47902@alunos.fc.ul.pt>
2019-11-11 15:43:47 +0000

    server logic added

--- server/src/main/java/ulisboa/tecnico/sirs/server/App.java
+++ server/src/main/java/ulisboa/tecnico/sirs/server/App.java
@@ -1,13 +1,47 @@
 package ulisboa.tecnico.sirs.server;
 
-/**
- * Hello world!
- *
- */
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+	public static void main( String[] args )
+	{
+		ServerSocket ss = null;
+		try {
+			ss = new ServerSocket(Integer.parseInt(args[0]));
+			while(true) {
+				try {
+					Socket inSoc = ss.accept();
+					ServerThread newServerThread = new ServerThread(inSoc);
+					newServerThread.start();
+				} catch(IOException e) {
+					e.printStackTrace();
+				}
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	class ServerThread extends Thread {
+		private Socket socket = null;
+		ObjectOutputStream outStream;
+		ObjectInputStream inStream;
+		private String currUser;
+
+		public ServerThread(Socket clientSocket) {
+			socket = clientSocket;
+			System.out.println("thread created for a new client");
+		}
+		
+		public void run() {
+			
+		}
+
+	}
+
 }
