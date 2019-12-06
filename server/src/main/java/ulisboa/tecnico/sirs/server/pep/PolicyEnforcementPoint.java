15d4ce24e0429275139a11f68adb95e143f06e2f
Miguel Grilo <miguel.ptfg@hotmail.com>
2019-12-06 19:31:13 +0000

    fix pep

--- server/src/main/java/ulisboa/tecnico/sirs/server/pep/PolicyEnforcementPoint.java
+++ server/src/main/java/ulisboa/tecnico/sirs/server/pep/PolicyEnforcementPoint.java
@@ -1,32 +1,10 @@
 package ulisboa.tecnico.sirs.server.pep;
 
-import static org.ow2.authzforce.xacml.identifiers.XacmlAttributeCategory.XACML_1_0_ACCESS_SUBJECT;
-
-import static org.ow2.authzforce.xacml.identifiers.XacmlAttributeCategory.XACML_3_0_ACTION;
-import static org.ow2.authzforce.xacml.identifiers.XacmlAttributeCategory.XACML_3_0_RESOURCE;
-
 import java.io.IOException;
-import java.util.Optional;
-
-import org.ow2.authzforce.core.pdp.impl.BasePdpEngine;
-import org.ow2.authzforce.core.pdp.impl.PdpEngineConfiguration;
-import org.ow2.authzforce.xacml.identifiers.XacmlAttributeId;
-
-import oasis.names.tc.xacml._3_0.core.schema.wd_17.DecisionType;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
 
 import org.casbin.jcasbin.main.Enforcer;
-/*import org.ow2.authzforce.core.pdp.api.AttributeFqn;
-import org.ow2.authzforce.core.pdp.api.AttributeFqns;
-import org.ow2.authzforce.core.pdp.api.DecisionRequest;
-import org.ow2.authzforce.core.pdp.api.DecisionRequestBuilder;
-import org.ow2.authzforce.core.pdp.api.DecisionResult;
-import org.ow2.authzforce.core.pdp.api.ImmutableDecisionRequest;
-import org.ow2.authzforce.core.pdp.api.value.AttributeBag;
-import org.ow2.authzforce.core.pdp.api.value.Bags;
-import org.ow2.authzforce.core.pdp.api.value.BooleanValue;
-import org.ow2.authzforce.core.pdp.api.value.StandardDatatypes;
-import org.ow2.authzforce.core.pdp.api.value.StringValue;
-import org.springframework.core.io.ClassPathResource;*/
 
 public class PolicyEnforcementPoint
 {
@@ -44,7 +22,10 @@ public class PolicyEnforcementPoint
 		
 		//this.pdp = new BasePdpEngine(pdpEngineConf);
 		
-		this.enforcer = new Enforcer("abac_model.conf");
+		
+		Path currentWorkingDirectory = FileSystems.getDefault().getPath("").toAbsolutePath();
+		System.out.println(currentWorkingDirectory);
+		this.enforcer = new Enforcer(currentWorkingDirectory + "/resources/abac_model.conf");
 		
 	}
 	
