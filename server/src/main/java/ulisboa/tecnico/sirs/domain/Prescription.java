4f239e851c17c21e9d104d0460baa613bdfe120a
Ana Nogueira <ana.nogueira.mu@gmail.com>
2019-12-08 20:46:08 +0000

    Fix Prescription class

--- server/src/main/java/ulisboa/tecnico/sirs/domain/Prescription.java
+++ server/src/main/java/ulisboa/tecnico/sirs/domain/Prescription.java
@@ -2,26 +2,14 @@ package ulisboa.tecnico.sirs.domain;
 
 import java.sql.Date;
 
-public class Medication {
+public class Prescription {
 	
-	private int id;
-	private int medicalRecordId;
 	private String name;
 	private Date prescriptionDate;
 	
-	public Medication(int id, int medicalRecordId, String name, Date prescriptionDate) {
-		this.id = id;
+	public Prescription(String name, Date prescriptionDate) {
 		this.name = name;
-		this.medicalRecordId = medicalRecordId;
 		this.prescriptionDate = prescriptionDate;
-	}
-
-	public int getId() {
-		return id;
-	}
-
-	public int getMedicalRecordId() {
-		return medicalRecordId;
 	}
 
 	public String getName() {
