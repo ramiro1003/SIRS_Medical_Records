de87addb3a09d567b5d3d545f433a6cc45f6c7b5
Ana Nogueira <ana.nogueira.mu@gmail.com>
2019-12-08 23:12:32 +0000

    add patient name

--- server/src/main/java/ulisboa/tecnico/sirs/domain/MedicalRecord.java
+++ server/src/main/java/ulisboa/tecnico/sirs/domain/MedicalRecord.java
@@ -5,24 +5,26 @@ import java.util.List;
 public class MedicalRecord {
 	
 	private Integer recordId, patientId, doctorId;
+	private String name;
 	private Integer height, weight;
 	private List<Prescription> prescription;
 	private List<Diagnosis> diagnosis;
 	private List<Treatment> treatment;
 	
 	
-	public MedicalRecord(Integer recordId, Integer patientId, Integer doctorId, Integer height, Integer weight,
+	public MedicalRecord(Integer recordId, Integer patientId, Integer doctorId, String name, Integer height, Integer weight,
 			List<Prescription> prescription, List<Diagnosis> diagnosis, List<Treatment> treatment) {
 		this.recordId = recordId;
 		this.patientId = patientId;
 		this.doctorId = doctorId;
+		this.name = name;
 		this.height = height;
 		this.weight = weight;
 		this.prescription = prescription;
 		this.diagnosis = diagnosis;
 		this.treatment = treatment;
 	}
-
+	
 
 	public Integer getRecordId() {
 		return recordId;
@@ -38,7 +40,12 @@ public class MedicalRecord {
 		return doctorId;
 	}
 
+	
+	public String getName() {
+		return name;
+	}
 
+	
 	public Integer getHeight() {
 		return height;
 	}
